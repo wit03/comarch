5bdb40e2b3cc117aa007dcbaff161b752a32f281
Jarukit Jintanasathilakul <witjarukit@gmail.com>
2024-01-31 15:49:04 +0700

    feat: intel and arm compile result

---
+++ lab2-assembly/test/arm/fibonacci.s
@@ -0,0 +1,75 @@
+	.cpu arm7tdmi
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"test_fibo.c"
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"Fibonacci Series: \000"
+	.align	2
+.LC1:
+	.ascii	"%d, \000"
+	.text
+	.align	2
+	.global	main
+	.arch armv4t
+	.syntax unified
+	.arm
+	.fpu softvfp
+	.type	main, %function
+main:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #16
+	mov	r3, #0
+	str	r3, [fp, #-12]
+	mov	r3, #1
+	str	r3, [fp, #-16]
+	ldr	r0, .L5
+	bl	printf
+	mov	r3, #1
+	str	r3, [fp, #-8]
+	b	.L2
+.L3:
+	ldr	r1, [fp, #-12]
+	ldr	r0, .L5+4
+	bl	printf
+	ldr	r2, [fp, #-12]
+	ldr	r3, [fp, #-16]
+	add	r3, r2, r3
+	str	r3, [fp, #-20]
+	ldr	r3, [fp, #-16]
+	str	r3, [fp, #-12]
+	ldr	r3, [fp, #-20]
+	str	r3, [fp, #-16]
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #1
+	str	r3, [fp, #-8]
+.L2:
+	ldr	r3, [fp, #-8]
+	cmp	r3, #10
+	ble	.L3
+	mov	r3, #0
+	mov	r0, r3
+	sub	sp, fp, #4
+	@ sp needed
+	pop	{fp, lr}
+	bx	lr
+.L6:
+	.align	2
+.L5:
+	.word	.LC0
+	.word	.LC1
+	.size	main, .-main
+	.ident	"GCC: (15:9-2019-q4-0ubuntu1) 9.2.1 20191025 (release) [ARM/arm-9-branch revision 277599]"
---
+++ lab2-assembly/test/arm/test2.s
@@ -0,0 +1,54 @@
+	.cpu arm7tdmi
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"test2.c"
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"%d\000"
+	.text
+	.align	2
+	.global	main
+	.arch armv4t
+	.syntax unified
+	.arm
+	.fpu softvfp
+	.type	main, %function
+main:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #16
+	mov	r3, #10
+	str	r3, [fp, #-8]
+	mov	r3, #20
+	str	r3, [fp, #-12]
+	ldr	r2, [fp, #-8]
+	ldr	r3, [fp, #-12]
+	add	r3, r2, r3
+	str	r3, [fp, #-16]
+	ldr	r1, [fp, #-16]
+	ldr	r0, .L3
+	bl	printf
+	mov	r3, #0
+	mov	r0, r3
+	sub	sp, fp, #4
+	@ sp needed
+	pop	{fp, lr}
+	bx	lr
+.L4:
+	.align	2
+.L3:
+	.word	.LC0
+	.size	main, .-main
+	.ident	"GCC: (15:9-2019-q4-0ubuntu1) 9.2.1 20191025 (release) [ARM/arm-9-branch revision 277599]"
